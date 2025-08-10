9352a22113d29056b3fc578868a79eea96cd8a77
rustjesty <soljesty.dev@gmail.com>
2025-08-10 12:18:25 +0900

    feat: add pumpfun create

--- src/dex/pumpfun.rs
+++ src/dex/pumpfun.rs
@@ -1,11 +1,13 @@
 use super::{
     amm_calc::{amm_buy_get_token_out, amm_sell_get_sol_out, calculate_with_slippage_buy, calculate_with_slippage_sell},
-    DexTrait, TokenAmountType,
+    dex_traits::DexTrait,
+    types::{Buy, Create, Sell, TokenAmountType},
 };
 use crate::{
     common::trading_endpoint::TradingEndpoint,
-    instruction::builder::{build_sell_instructions, build_wsol_buy_instructions, PriorityFee},
+    instruction::builder::{build_buy_instructions, build_sell_instructions, PriorityFee},
 };
+use borsh::{BorshDeserialize, BorshSerialize};
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 use solana_sdk::{
@@ -29,6 +31,14 @@ pub const MINT_AUTHORITY_SEED: &[u8] = b"mint-authority";
 pub const BONDING_CURVE_SEED: &[u8] = b"bonding-curve";
 pub const CREATOR_VAULT_SEED: &[u8] = b"creator-vault";
 pub const METADATA_SEED: &[u8] = b"metadata";
+
+pub const INITIAL_VIRTUAL_TOKEN_RESERVES: u64 = 1_073_000_000_000_000;
+pub const INITIAL_VIRTUAL_SOL_RESERVES: u64 = 30_000_000_000;
+
+lazy_static::lazy_static! {
+    static ref PUBKEY_MINT_AUTHORITY_PDA: Pubkey = Pubkey::find_program_address(&[MINT_AUTHORITY_SEED], &PUBKEY_PUMPFUN).0;
+    static ref PUBKEY_GLOBAL_PDA: Pubkey = Pubkey::find_program_address(&[GLOBAL_SEED], &PUBKEY_PUMPFUN).0;
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GlobalAccount {
@@ -55,6 +65,61 @@ pub struct BondingCurveAccount {
     pub creator: Pubkey,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub struct BuyInfo {
+    pub discriminator: u64,
+    pub token_amount: u64,
+    pub sol_amount: u64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub struct SellInfo {
+    pub discriminator: u64,
+    pub token_amount: u64,
+    pub sol_amount: u64,
+}
+
+impl From<Buy> for BuyInfo {
+    fn from(buy: Buy) -> Self {
+        Self {
+            discriminator: 16927863322537952870,
+            token_amount: buy.token_amount,
+            sol_amount: buy.sol_amount,
+        }
+    }
+}
+
+impl From<Sell> for SellInfo {
+    fn from(sell: Sell) -> Self {
+        Self {
+            discriminator: 12502976635542562355,
+            token_amount: sell.token_amount,
+            sol_amount: sell.sol_amount,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub struct CreateInfo {
+    pub discriminator: u64,
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
+    pub creator: Pubkey,
+}
+
+impl CreateInfo {
+    pub fn from_create(create: Create, creator: Pubkey) -> Self {
+        Self {
+            discriminator: 8576854823835016728,
+            name: create.name,
+            symbol: create.symbol,
+            uri: create.uri,
+            creator,
+        }
+    }
+}
+
 pub struct Pumpfun {
     pub endpoint: Arc<TradingEndpoint>,
     pub global_account: OnceCell<Arc<GlobalAccount>>,
@@ -78,8 +143,60 @@ impl DexTrait for Pumpfun {
         Ok(())
     }
 
-    fn create(&self, _: u64) -> anyhow::Result<u64> {
-        Err(anyhow::anyhow!("Not supported"))
+    async fn create(&self, payer: Keypair, create: Create, fee: Option<PriorityFee>, tip: Option<u64>) -> anyhow::Result<Vec<Signature>> {
+        let mint = create.mint;
+        let buy_sol_amount = create.buy_sol_amount;
+        let create_info = CreateInfo::from_create(create, payer.pubkey());
+        let mut buffer = Vec::new();
+        create_info.serialize(&mut buffer)?;
+
+        let blockhash = self.endpoint.rpc.get_latest_blockhash().await?;
+        let bonding_curve = Self::get_bonding_curve_pda(&mint).ok_or(anyhow::anyhow!("Bonding curve not found"))?;
+
+        let mut instructions = vec![];
+        let create_instruction = Instruction::new_with_bytes(
+            PUBKEY_PUMPFUN,
+            &buffer,
+            vec![
+                AccountMeta::new(mint, true),
+                AccountMeta::new(*PUBKEY_MINT_AUTHORITY_PDA, false),
+                AccountMeta::new(bonding_curve, false),
+                AccountMeta::new_readonly(*PUBKEY_GLOBAL_PDA, false),
+                AccountMeta::new_readonly(mpl_token_metadata::ID, false),
+                AccountMeta::new(mpl_token_metadata::accounts::Metadata::find_pda(&mint).0, false),
+                AccountMeta::new(payer.pubkey(), true),
+                AccountMeta::new(payer.pubkey(), true),
+                AccountMeta::new_readonly(solana_program::system_program::ID, false),
+                AccountMeta::new_readonly(spl_token::ID, false),
+                AccountMeta::new_readonly(spl_associated_token_account::ID, false),
+                AccountMeta::new_readonly(solana_program::sysvar::rent::ID, false),
+                AccountMeta::new_readonly(PUBKEY_EVENT_AUTHORITY, false),
+                AccountMeta::new_readonly(PUBKEY_PUMPFUN, false),
+            ],
+        );
+
+        instructions.push(create_instruction);
+
+        if let Some(buy_sol_amount) = buy_sol_amount {
+            let buy_token_amount = amm_buy_get_token_out(INITIAL_VIRTUAL_SOL_RESERVES, INITIAL_VIRTUAL_TOKEN_RESERVES, buy_sol_amount);
+            let creator_vault = Self::get_creator_vault_pda(&payer.pubkey()).unwrap();
+            let bonding_curve = Self::get_bonding_curve_pda(&mint).ok_or(anyhow::anyhow!("Bonding curve not found"))?;
+            let buy_instruction = self.build_buy_instruction(
+                &payer,
+                &mint,
+                &bonding_curve,
+                &creator_vault,
+                Buy {
+                    token_amount: buy_token_amount,
+                    sol_amount: buy_sol_amount,
+                },
+            )?;
+            instructions.push(buy_instruction);
+        }
+
+        let signatures = self.endpoint.send_transactions(&payer, instructions, blockhash, fee, tip).await?;
+
+        Ok(signatures)
     }
 
     async fn buy(
@@ -120,7 +237,7 @@ impl DexTrait for Pumpfun {
         mint: &Pubkey,
         pool: &Pubkey,
         creator: Option<&Pubkey>,
-        sol_lamports: u64,
+        sol_amount: u64,
         buy_token_amount: u64,
         blockhash: Hash,
         fee: Option<PriorityFee>,
@@ -128,8 +245,17 @@ impl DexTrait for Pumpfun {
     ) -> anyhow::Result<Vec<Signature>> {
         let creator = creator.ok_or(anyhow::anyhow!("Creator not provided"))?;
         let creator_vault = Self::get_creator_vault_pda(creator).unwrap();
-        let instruction = self.build_buy_instruction(payer, mint, &pool, &creator_vault, buy_token_amount, sol_lamports)?;
-        let instructions = build_wsol_buy_instructions(payer, mint, sol_lamports, instruction)?;
+        let instruction = self.build_buy_instruction(
+            payer,
+            mint,
+            &pool,
+            &creator_vault,
+            Buy {
+                token_amount: buy_token_amount,
+                sol_amount,
+            },
+        )?;
+        let instructions = build_buy_instructions(payer, mint, instruction)?;
         let signatures = self.endpoint.send_transactions(payer, instructions, blockhash, fee, tip).await?;
 
         Ok(signatures)
@@ -173,7 +299,7 @@ impl DexTrait for Pumpfun {
         pool: &Pubkey,
         creator: Option<&Pubkey>,
         token_amount: u64,
-        sol_lamports: u64,
+        sol_amount: u64,
         close_mint_ata: bool,
         blockhash: Hash,
         fee: Option<PriorityFee>,
@@ -181,7 +307,7 @@ impl DexTrait for Pumpfun {
     ) -> anyhow::Result<Vec<Signature>> {
         let creator = creator.ok_or(anyhow::anyhow!("Creator not provided"))?;
         let creator_vault = Self::get_creator_vault_pda(creator).unwrap();
-        let instruction = self.build_sell_instruction(payer, mint, &pool, &creator_vault, token_amount, sol_lamports)?;
+        let instruction = self.build_sell_instruction(payer, mint, &pool, &creator_vault, Sell { token_amount, sol_amount })?;
         let instructions = build_sell_instructions(payer, mint, instruction, close_mint_ata)?;
         let signatures = self.endpoint.send_transactions(payer, instructions, blockhash, fee, tip).await?;
 
@@ -223,25 +349,16 @@ impl Pumpfun {
         Ok((bonding_curve_pda, bonding_curve))
     }
 
-    fn build_buy_instruction(
-        &self,
-        payer: &Keypair,
-        mint: &Pubkey,
-        bonding_curve: &Pubkey,
-        creator_vault: &Pubkey,
-        buy_token_amount: u64,
-        max_sol_cost: u64,
-    ) -> anyhow::Result<Instruction> {
+    fn build_buy_instruction(&self, payer: &Keypair, mint: &Pubkey, bonding_curve: &Pubkey, creator_vault: &Pubkey, buy: Buy) -> anyhow::Result<Instruction> {
         self.initialized()?;
 
-        let mut data = Vec::with_capacity(8 + 8 + 8);
-        data.extend_from_slice(&[102, 6, 61, 18, 1, 218, 235, 234]); // discriminator
-        data.extend_from_slice(&buy_token_amount.to_le_bytes());
-        data.extend_from_slice(&max_sol_cost.to_le_bytes());
+        let buy_info: BuyInfo = buy.into();
+        let mut buffer = Vec::new();
+        buy_info.serialize(&mut buffer)?;
 
         Ok(Instruction::new_with_bytes(
             PUBKEY_PUMPFUN,
-            &data,
+            &buffer,
             vec![
                 AccountMeta::new_readonly(PUBKEY_GLOBAL_ACCOUNT, false),
                 AccountMeta::new(PUBKEY_FEE_RECIPIENT, false),
@@ -265,17 +382,15 @@ impl Pumpfun {
         mint: &Pubkey,
         bonding_curve: &Pubkey,
         creator_vault: &Pubkey,
-        token_amount: u64,
-        min_sol_out: u64,
+        sell: Sell,
     ) -> anyhow::Result<Instruction> {
-        let mut data = Vec::with_capacity(8 + 8 + 8);
-        data.extend_from_slice(&[51, 230, 133, 164, 1, 127, 131, 173]); // discriminator
-        data.extend_from_slice(&token_amount.to_le_bytes());
-        data.extend_from_slice(&min_sol_out.to_le_bytes());
+        let sell_info: SellInfo = sell.into();
+        let mut buffer = Vec::new();
+        sell_info.serialize(&mut buffer)?;
 
         Ok(Instruction::new_with_bytes(
             PUBKEY_PUMPFUN,
-            &data,
+            &buffer,
             vec![
                 AccountMeta::new_readonly(PUBKEY_GLOBAL_ACCOUNT, false),
                 AccountMeta::new(PUBKEY_FEE_RECIPIENT, false),
